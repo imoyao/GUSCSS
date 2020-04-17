7a9205f92a92c98e85d03879dfeee458ee4c2546
imoyao <emailme8@163.com>
2020-04-17 16:09:49 +0800

    add docs

---
+++ docs/precache-manifest.9af87c777cd06087dba18bb141eb7833.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/nippon-color/robots.txt"
+  },
+  {
+    "revision": "16dcc062105272e289d5",
+    "url": "/nippon-color/js/chunk-vendors.2b4cb03c.js"
+  },
+  {
+    "revision": "4ce7767189a60a739c2b",
+    "url": "/nippon-color/js/app.f1b3d6f2.js"
+  },
+  {
+    "revision": "5a33a41e411b0aeb6f3b",
+    "url": "/nippon-color/js/about.9f8037cf.js"
+  },
+  {
+    "revision": "1559262fb19293c086e37648faa29a2e",
+    "url": "/nippon-color/index.html"
+  },
+  {
+    "revision": "c9ffbd3c127fc4e2273d3302cd40ba6b",
+    "url": "/nippon-color/img/64253519_p9.c9ffbd3c.png"
+  },
+  {
+    "revision": "b54703139d3c8c811a75a2e44906f3b9",
+    "url": "/nippon-color/fonts/XANO-mincho-U32.b5470313.ttf"
+  },
+  {
+    "revision": "c53a6bd35c13f5f094e41804d6814143",
+    "url": "/nippon-color/fonts/BPtypewrite.c53a6bd3.ttf"
+  },
+  {
+    "revision": "16dcc062105272e289d5",
+    "url": "/nippon-color/css/chunk-vendors.7f4df90b.css"
+  },
+  {
+    "revision": "4ce7767189a60a739c2b",
+    "url": "/nippon-color/css/app.68e0f032.css"
+  }
+];
